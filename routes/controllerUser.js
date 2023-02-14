a7b980a85e89cd99f76558b46351a4e3ed428c21
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-02-13 22:29:13 -0500

    cambio de apariencia, se agrega efecto blur

--- routes/controllerUser.js
+++ routes/controllerUser.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const {static}= require('express')
 const router = Router()
 
 router.post('/login',(req,resp)=>{  
@@ -10,9 +11,13 @@ router.post('/registro',(req,resp)=>{
     return resp.json({"username":req.body['username'],"password":true})
  })
  
- 
- 
-router.get('/HomPage',(req,resp)=>{
+
+router.use(static('./storage/FotosPerfil'))//mainmidler 
+router.get('/HomPage', async(req,resp)=>{
+
+   const respuesta = await fetch('https://jsonplaceholder.typicode.com/photos').then(data=>data.json())
+    
+    
     imagenes =  {"imagenes":["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg","13.jpg","14.jpg","14.jpg"]}
     resp.render('homePage',imagenes)
  })
