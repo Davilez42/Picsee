8af7989f30d4c2a2dbf68cb68bb72c009761bf99
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-02-14 17:17:53 -0500

    modifico consulta para obtener el id_avatar

--- models/RepositorioUsers.js
+++ models/RepositorioUsers.js
@@ -3,7 +3,7 @@ const getConection = require("./ConfigDataBase");
 
 const get_user_Loguin =async(username) => { 
     const conection = await  getConection()
-    const result = await conection.execute(`SELECT id_user,username,passwrd from users WHERE username = '${username}'`)
+    const result = await conection.execute(`SELECT id_user,id_avatar,username,passwrd from users WHERE username = '${username}'`)
     if (result[0].length ==0) {//verifico si el resultado no contiene nada
         return null        
     }
