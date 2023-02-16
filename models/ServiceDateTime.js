8fba093e9df5f720b652d7ededfc94663e0f1552
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-02-16 12:05:23 -0500

    se agrega opcion de eliminar cuenta al front y se agregar servicion de fechas

---
+++ models/ServiceDateTime.js
@@ -0,0 +1,14 @@
+const getDateTimeNow=()=>{
+    const date = new Date()
+    const año = date.getFullYear()
+    const dia = date.getDate().toString().length ==1 ? "0".concat(date.getDate()):date.getDate().toString()
+    const mes = date.toLocaleDateString().split('/')[1].length ==1 ? "0".concat(date.toLocaleDateString().split('/')[1]):date.toLocaleDateString().split('/')[1].toString()
+    const hora = date.toLocaleTimeString().split('p')[0]
+    return `${año}-${mes}-${dia} ${hora}`
+}
+
+
+
+module.exports = {
+    getDateTimeNow
+}
