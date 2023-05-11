601296b38fb7e7728e00f1ab4741357e128f1f7e
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-05-11 14:01:13 -0500

    se agrega un render para mejorar respuesta al cliente

--- models/ServiceWebAccessToken.js
+++ models/ServiceWebAccessToken.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-
 const generateAccessToken = (data) => {
   return jwt.sign(data, process.env.KEY_SECRET, { expiresIn: "1h" });
 };
@@ -9,13 +8,14 @@ const validateToken = (req, res, next) => {
   const token = req.query.t_ken || req.headers["auth"];
   console.log("TOKEN EN SERVIDOR",token)
   if (!token) {
-    res.sendStatus(403);
+    res.render('info.ejs',{message:"Acceso Denegado,No se meta donde no debe deje de joder mka🤣"})
     return;
   }
 
   jwt.verify(token, process.env.KEY_SECRET, (err, user) => {
     if (err) {
-      res.sendStatus(400);
+      console.log("token vencido")
+      res.render('info.ejs',{message:"Tu sesion ha caducado.. inicia sesion nuevamente 🤣"})
     } else {
       next();
     }
