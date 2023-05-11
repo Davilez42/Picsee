4c3293290513c77ecb068d778328aa66dce2ff4d
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-05-11 14:03:22 -0500

    mejora de peticion a /HomPage

--- public/js/cargarSesion.js
+++ public/js/cargarSesion.js
@@ -3,7 +3,13 @@ const cargar_informacion= async ()=>{
         const user_logged = localStorage.getItem('loggedUser');
         if(user_logged!=undefined){
            const token =  JSON.parse(user_logged).token
-           window.location.href=`/HomPage?t_ken=${token}`;       
+         //window.location.href=`/HomPage?t_ken=${token}`;       
+        fetch(`/HomPage?t_ken=${token}`,{method:"GET"}).then(x=>{
+                window.location.href = x.url
+        }).catch((res)=>{
+               console.log("entra aqui al error")
+               
+        })
         }
     
 }
