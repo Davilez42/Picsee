409883140a8819bddd83009a028c82ac12aa7213
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-05-11 13:59:10 -0500

    se agrega funcion para eliminar imagenes en la bd

--- models/RepositorioImages.js
+++ models/RepositorioImages.js
@@ -6,9 +6,6 @@ const setImage = async(name_file)=>{
     let file_name = name_file.split('.')
     const exten = file_name.pop()
     file_name = file_name.join(".")
-    console.log(file_name)
-
-   
     return  conection.execute(`Insert into artgalery.images (name,format_) VALUES("${file_name}","${exten}")`)
     .then(async()=>{
      return conection.execute(`Select id_image from images where name="${file_name}"`).then(date=> date[0][0].id_image)
@@ -19,7 +16,7 @@ const setImage = async(name_file)=>{
     
   }
 
-const deleteImages= async(user)=>{
+const getImagesById= async(user)=>{
 const conection = await getConection();
 return conection.execute(`Select  concat( i.id_image,"_",i.name,".", i.format_)  AS f_name 
                       from posts p
@@ -27,4 +24,21 @@ return conection.execute(`Select  concat( i.id_image,"_",i.name,".", i.format_) 
                       where p.id_user = ${user}`).then(data=>data[0]);
 }  
 
-module.exports = {setImage,deleteImages};
+
+const deleteImages= async(images)=>{
+  
+  if(images.length==0){
+    return
+  }
+  
+  let consulta = []
+  for (const nma of images) {
+      consulta.push(nma.f_name.split("_")[0])
+  }
+ // console.log(consulta)
+  consulta = "("+consulta.join(",")+")"
+  const conection = await getConection();
+  return conection.execute(`DELETE from images where id_image in ${consulta}`)
+}
+
+module.exports = {setImage,getImagesById,deleteImages};
