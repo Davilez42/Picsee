b713c9a966d0c54d541ffb7511ddf580649be1cf
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-07-04 07:54:39 -0500

    _d

--- tests/ServiceEncrypted.test.js
+++
@@ -1,7 +0,0 @@
-const ServiceEncrypted = require('../src/services/encrypted.service')
-
-test('test function set hastag on bd',async ()=>{
-    const valor  = await ServiceEncrypted.encrypted('123456789')
-    const result = await ServiceEncrypted.compare_(valor,'123456789')
-    console.log(result);
-});
--- tests/serviceTime.test.js
+++
@@ -1,6 +0,0 @@
-const {getDateTimeNow} = require('../src/services/dateTime.service')
-test('test function getDateTimeNow',async ()=>{
-
-    console.log(getDateTimeNow());
-
-});
