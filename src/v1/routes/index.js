5fdd611bfe9eb78d006d3421ee8b8e0760355793
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-07-12 20:46:55 -0500

    refactor:

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -1,6 +1,5 @@
 const Router =  require('express')
 const authRoutes = require('./authRoutes')
-const postsRoutes = require('./postsRoutes')
 const userRoutes =  require('./userRoutes')
 
 const router =  Router()
@@ -8,6 +7,5 @@ const router =  Router()
 router.get('/',(req,res)=>{return res.json({"message":"Welcome To routes v1 !"})})
 router.use(authRoutes)
 router.use(userRoutes)
-
 
 module.exports =  router
