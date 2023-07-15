a8d57c8e38e45c4eadfca4648ce5efb58ea1ec6e
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-07-14 22:23:53 -0500

    feat(controller):auth with plataform

--- src/controllers/authPlataformController.js
+++ src/controllers/authPlataformController.js
@@ -1,26 +1,47 @@
-const jwt = require('jsonwebtoken')
-const userServices = require('../services/users.service')
-require('dotenv').config()
-const authUser = async(req,res)=>{
-    try {
-        const {credential} =  req.body
-        const data_decoded =  jwt.decode(credential,process.env.SECRET_KEY_GOOGLE)
-        console.log(data_decoded);
-        let user_db = await userServices.get_user_Loguin(data_decoded.name)
-        if(!user_db){
-            ///TODO:creo esquema de usuario y para insertarlo en la base de datos 
-            //despues retorno el usuario con su token generado
-            const user = {
-                username:data_decoded.name,
-                id_avata
-            }
-            const insert_id =  await userServices.insert_user({username:})
-        }
-        res.status(200).json(data_decoded)      
-    } catch (error) {
-        res.status(500).json({messageError:error.message})
+const jwt = require("jsonwebtoken");
+const userServices = require("../services/users.service");
+const avatarUserServices = require("../services/avatarsUsers.service");
+const genareteToken = require("../services/generateToken.service");
+const { json } = require("express");
+require("dotenv").config();
+const authUser = async (req, res) => {
+  try {
+    const { credential, g_csrf_token } = req.body;
+
+    if (!credential) {
+      return res.status(400).json({ message: "params is incorrect!" });
     }
-}
 
 
-module.exports =  {authUser}
+    const { name, email, picture } = jwt.decode(credential,process.env.SECRET_KEY_GOOGLE);
+
+    let user_ = await userServices.get_user_Loguin(name);
+
+    const user = {
+      username: name,
+      email: email,
+      first_names: " ",
+      last_names: " ",
+      password: "true",
+    };
+
+    if (!user_[0]) {
+      const insert_id = await userServices.insert_user(user);
+      avatarUserServices.insertAvatar(insert_id, picture); //inserto avatar
+      user.id_user = insert_id;
+    }
+
+    if (!user.id_user) {
+      user.id_user = user_[0].id_user;
+    }
+    user.username = [true, name];
+
+    const token = genareteToken(user);
+
+    res.status(200).json({ token, ...user });
+  } catch (error) {
+    res.status(500).json({ messageError: error.message });
+  }
+};
+
+module.exports = { authUser };
--- src/v1/routes/authRoutes.js
+++ src/v1/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const Router = require('express')
 const authController = require('../../controllers/authController')
+const authPlataformController = require('../../controllers/authPlataformController')
 const router = Router()
 
 
 router.post('/validateUser',authController.valdiateUser)
 router.post('/registerUser',authController.resgiterUser)
+router.post('/auth_plataform',authPlataformController.authUser)
 
 module.exports = router
