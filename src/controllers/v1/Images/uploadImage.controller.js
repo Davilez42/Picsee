9177eca49f822ac29ffaf86dcfd215054ac2fede
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-08-01 20:11:49 -0500

    feat(api): moderator of images

--- src/controllers/v1/Images/uploadImage.controller.js
+++ src/controllers/v1/Images/uploadImage.controller.js
@@ -3,20 +3,36 @@ const RepositorioImages = require("../../../database/images.service.js");
 const RepositorioHastags = require("../../../database/hastags.service.js");
 const upload_Images_Cdn = require("../../../microservices/imageKit/uploadImages.service.js");
 const { IMAGE_KIT_CONFIG } = require("../../../../configs/config.js");
+const moderator = require("../../../microservices/ImageModerator/imageModerator.js");
 const uploadImage = async (req, res) => {
   //* controller for upload image to cdn
 
   const { id_user } = req.params;
-  let files = req.files.archivo;
-  let hastags = req.headers.hastags.split(",");
 
   try {
+    let files = req.files.archivo;
+
+    let hastags = req.headers.hastags.split(",");
     if (hastags.length == 1 && hastags[0].length == 0) {
       hastags = null;
     }
     if (!files.length) {
       files = [files];
     }
+    //* verify images moderator
+
+    for (const f of files) {
+      if (! await moderator(f)) {
+        return res
+          .status(400)
+          .json({
+            errorMessage:
+              "Las imagenes no se han subido debido a que son contenido explicito",
+          });
+      }
+
+    }
+
     const id_images_cdn = await upload_Images_Cdn(
       files,
       IMAGE_KIT_CONFIG.images_folder_dest
@@ -27,7 +43,7 @@ const uploadImage = async (req, res) => {
     const id_posts = await RepositorioPost.setPosts(id_user, ids_image, 1);
 
     if (hastags != null) {
-      await RepositorioHastags.setHastags(id_posts,hastags);
+      await RepositorioHastags.setHastags(id_posts, hastags);
     }
 
     res.sendStatus(204);
---
+++ src/microservices/ImageModerator/imageModerator.js
@@ -0,0 +1,44 @@
+const request = require('postman-request')
+const { createReadStream } = require('fs')
+const FormData = require('form-data');
+const { join } = require('path')
+const moderator = async (file) => {
+
+    const API_USER = '1954188604'
+    const API_SECRET = 'Z68A4TGKspPNPYzQGQyk'
+
+    const route = join(__dirname, 'temp/tesPic.jpg')
+    file.mv(route, (err) => err)
+
+    const formData = new FormData();
+    formData.append('models', 'nudity-2.0,wad,offensive,face-attributes,gore');
+    formData.append('media', createReadStream(route));
+    formData.append('api_user', API_USER);
+    formData.append('api_secret', API_SECRET);
+
+
+    const data = {
+        url: 'https://api.sightengine.com/1.0/check.json',
+        body: formData,
+        headers: formData.getHeaders(),
+    }
+
+
+    return new Promise((resolve, reject) => {
+        request.post(data, (err, httpres, body) => {
+            if (err) {
+                return reject(err)
+            }
+            const { status, nudity, gore, weapon, drugs, medical_drugs, skull } = JSON.parse(body)
+
+            if (status == "failure") {
+                return reject('Error')
+            }
+            const porcentages = [nudity.sexual_activity, nudity.sexual_display, nudity.erotica, gore.prob, weapon, drugs, medical_drugs, skull.prob]
+            const pors = porcentages.filter(d => d > 0.20);
+            (pors.length == 0) ? resolve(true) : resolve(false)
+        })
+
+    })
+}
+module.exports = moderator
