0482de07bbc6717ab40b2dabed4b5da3590d42c3
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-08-05 16:12:15 -0500

    fix:

--- src/middlewares/handlerError.middleware.js
+++ src/middlewares/handlerError.middleware.js
@@ -1,9 +1,5 @@
-const handlerError = (err, req, res) => {
+const handlerError = (req, res) => {
 
-  /*   if (err) {
-      //console.log(err.message)
-    }
-   */
   res.status(404).render("info.ejs", { message: "🔍 La ruta Solicitada no existe ❗" });
 };
 module.exports = handlerError;
